670a1fdb596d039bf6565d1c2639988c81bd5389
sdquang75 <sdquang75@gmail.com>
2025-04-21 00:48:22 +0900

    thay doi nho

--- src/Components/Routers/Login/Login.jsx
+++ src/Components/Routers/Login/Login.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
-const Login = () => {
+const Login = () => {}
+const handleChange = (e) => {
+
   return (
     <div>
-      
+    <div className='login-page'>
+      <h1>ログイン画面</h1>
+      <div className='login-container'>
+        <div className='form-group'>
+        <label htmlFor="email">社員番号</label>
+        <input
+              type="loginemail"
+              id="email"
+              name="email"
+              placeholder="example@domain.com"
+              value={formData.email}
+              // onChange={handleChange}
+              required
+            />
+        </div>
+        <div className='form-group'>
+          <label htmlFor="password">パスワード</label>
+          <input
+                type="loginpassword"
+                id="password"
+                name="password"
+                placeholder="パスワード"
+                value={formData.password}
+                // onChange={handleChange}
+                required
+              />
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
